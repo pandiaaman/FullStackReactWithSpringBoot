a8657344de04c176e6034264f8eaf8bd00004ee9
Aman Pandia <pandiaaman16@gmail.com>
2023-08-08 21:17:20 +0530

    pom.xml update

--- OreillyFullStackAppBak/src/main/java/com/pandiaaman/bak/OreillyFullStackAppBakApplication.java
+++ OreillyFullStackAppBak/src/main/java/com/pandiaaman/bak/OreillyFullStackAppBakApplication.java
@@ -3,11 +3,17 @@ package com.pandiaaman.bak;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class OreillyFullStackAppBakApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OreillyFullStackAppBakApplication.class, args);
+		log.info("*************************************************");
+		log.info("bak app started");
+		log.info("*************************************************");
 	}
 
 }
