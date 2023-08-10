2e57cec52f91df8a29a916051406069dcb1429fc
Aman Pandia <pandiaaman16@gmail.com>
2023-08-11 03:04:37 +0530

    rest client app update

--- OreillyFullStackAppBakClient/src/main/java/com/pandiaaman/bakclient/MyRestClient.java
+++ OreillyFullStackAppBakClient/src/main/java/com/pandiaaman/bakclient/MyRestClient.java
@@ -1,5 +1,24 @@
 package com.pandiaaman.bakclient;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.pandiaaman.bakclient.models.Product;
+
+@Component
 public class MyRestClient {
 
+	private String baseAddress = "http://localhost:8081/products/";
+	
+	@Autowired
+	private RestTemplate template;
+	
+	public boolean demoInsertProduct(String name, double price) {
+		Product prod = new Product("-1", name, price);
+		
+		ResponseEntity<Product> response = template.postForEntity(baseAddress, prod, Product.class);
+		return true;
+	}
 }
